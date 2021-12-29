e9eaf9f6f98faa9c7e9d1a7dc20d427a788c2133
minyekai <490231923@qq.com>
2021-12-29 21:31:02 +0800

    first

---
+++ src/main/java/com/vsuni/git.java
@@ -0,0 +1,11 @@
+package com.vsuni;
+
+/**
+ * @author minyekai
+ * @date Created in 21:29 2021/12/29 0029
+ */
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
